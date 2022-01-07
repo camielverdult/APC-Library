12cea3dd8c9813868f927a7bc77953edce7afdd4
camielverdult <incredabox@gmail.com>
2022-01-07 16:13:19 +0100

    Update pair.h

--- standard/pair.h
+++ standard/pair.h
@@ -5,19 +5,25 @@
 #ifndef APC_LIBRARY_PAIR_H
 #define APC_LIBRARY_PAIR_H
 
-template <typename Key, typename Value>
+template <typename T1, typename T2>
 class pair {
-    using key_type = Key;
-    using key_pointer = Key*;
-    using key_reference = Key&;
-    using key_const_reference = const Key&;
 
-    using value_type = Value;
-    using value_pointer = Value*;
-    using value_reference = Value&;
-    using value_const_reference = const Value&;
+public:
+    using first_type = T1;
+    using first_pointer = T1*;
+    using first_reference = T1&;
+    using first_const_reference = const T1&;
 
-    
+    using second_type = T2;
+    using second_pointer = T2*;
+    using second_reference = T2&;
+    using second_const_reference = const T2&;
+
+    pair(T1 first, T2 second): m_first {first}, m_second (second) {}
+
+private:
+    T1* m_first;
+    T2* m_second;
 };
 
 #endif //APC_LIBRARY_PAIR_H
