7fcede4a66708e9b5e8f183e73df363ebf5ec11e
camielverdult <incredabox@gmail.com>
2022-01-07 16:08:07 +0100

    Add stuff to main and to pair header

--- main.cpp
+++ main.cpp
@@ -2,3 +2,7 @@
 // Created by Camiel Verdult on 1/7/2022.
 //
 
+int main() {
+
+    return 0;
+}
--- standard/pair.h
+++ standard/pair.h
@@ -5,4 +5,19 @@
 #ifndef APC_LIBRARY_PAIR_H
 #define APC_LIBRARY_PAIR_H
 
+template <typename Key, typename Value>
+class pair {
+    using key_type = Key;
+    using key_pointer = Key*;
+    using key_reference = Key&;
+    using key_const_reference = const Key&;
+
+    using value_type = Value;
+    using value_pointer = Value*;
+    using value_reference = Value&;
+    using value_const_reference = const Value&;
+
+    
+};
+
 #endif //APC_LIBRARY_PAIR_H
