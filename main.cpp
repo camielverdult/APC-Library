25835509894b209baf7b18616523b02aeef73fa5
marnixlaar <74597221+marnixlaar@users.noreply.github.com>
2022-01-15 15:23:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,97 +16,111 @@ int main() {
      * Please take a look at the header for all functions, some are not used here
      */
 
-    mc::map<std::string, uint64_t> values;
+//    mc::map<std::string, uint64_t> values;
+//
+//    values.push_back(mc::pair<std::string, uint64_t>{"Hi", 1});
+//    std::cout << values << "\n";
+//
+////    mc::vector<int> test{1, 2, 3, 4};
+////
+////    test.insert(0, 0);
+////
+////    return 0;
+//
+//    mc::pair pairOfVectorInt{std::vector<std::string>{"hallo"}, 1};
+//
+//    mc::pair pairOfInts{1, 2};
+//
+//    mc::pair pairOfStrings{std::string("Hello"), std::string("World!")};
+//
+//    std::vector<mc::pair<std::string, int>> people{
+//        mc::pair<std::string, int>{"Marnix", 20},
+//        mc::pair<std::string, int>{"Camiel", 20}
+//    };
+//
+//    std::cout << "People: " << std::endl;
+//
+//    for (auto& person : people) {
+//        std::cout << person << std::endl;
+//    }
+//
+//    // This won't work! The types are different for both first and second
+//    // pairOfInts.swap();
+//
+//    // This will work fine on the other hand
+//    std::cout << "Swap print: " << mc::pair<int, int>{1, 20}.swap() << std::endl;
+//
+//    // Test our vector!
+//    mc::vector<std::string> vec{};
+//    vec.push_back("Hello");
+//    vec.push_back("Alice.");
+//    vec.push_back("How");
+//    vec.push_back("are");
+//    vec.push_back("you");
+//    vec.push_back("today?");
+//
+//    std::cout << "Vector print: " << vec << std::endl;
+//
+//    mc::pair<std::string, int> marnix{"Marnix", 20};
+//    mc::pair<std::string, int> camiel{"Camiel", 20};
+//
+//    mc::pair<int, std::string> setie;
+//
+//    mc::pair<std::string, int> pairArray[10];
+//
+//    pairArray[1] = camiel;
+//
+//    std::cout << pairArray[1] << std::endl;
+//
+//    mc::vector<mc::pair<std::string, int>> set;
+//
+//    set.push_back(mc::pair<std::string, int>{"One", 1});
+//    set.push_back(mc::pair<std::string, int>{"Two", 2});
+//
+//    // Delete the second pair out of our vector
+//    set.erase(1);
+//    std::cout << set << std::endl;
+//
+//    // Fill an array with random numbers and sort them
+//    mc::vector<uint32_t> unsorted;
+//
+//    // Use mersenne twister to generate random numbers, see our previous topic
+//    // For more information about the working of the mersenne twister
+//    std::mt19937 mtw;
+//    mtw.seed((int)time(nullptr));
+//
+//    int iterations = 100000;
+//
+//    std::cout << "Iterations: " << iterations << std::endl;
+//
+//    for (int i = 0; i < iterations; i++) {
+//        unsorted.push_back(mtw());
+//    }
+//
+//    auto start = std::chrono::high_resolution_clock::now();
+//
+//    unsorted.sort();
+//
+//    auto finish = std::chrono::high_resolution_clock::now();
+//    std::cout << "Sorted " << unsorted.size() << " elements in " << std::chrono::duration_cast<std::chrono::milliseconds>(finish-start).count() << " ms\n";
+//
+//    return 0;
 
-    values.push_back(mc::pair<std::string, uint64_t>{"Hi", 1});
-    std::cout << values << "\n";
+    std::pair<int,std::string> a{1,"hans"};
+    std::pair<int,std::string> b{2,"peter"};
+    std::pair<int,std::string> c{3,"karel"};
+    std::pair<int,std::string> d{4,"piet"};
+    std::pair<int,std::string> e{5,"jan"};
 
-    mc::vector<int> test{1, 2, 3, 4};
+    std::vector<std::pair<int,std::string>> guys{a,b,e,d,c};
 
-    test.insert(0, 0);
+    std::cout << guys[0].second << ", " << guys[1].second << ", " << guys[2].second << ", " << guys[3].second << ", " << guys[4].second << std::endl;
+
+    std::sort(guys.begin(), guys.end(), [&](auto a, auto b){
+        return a < b;
+    });
+
+    std::cout << guys[0].second << ", " << guys[1].second << ", " << guys[2].second << ", " << guys[3].second << ", " << guys[4].second << std::endl;
 
     return 0;
-
-    mc::pair pairOfVectorInt{std::vector<std::string>{"hallo"}, 1};
-
-    mc::pair pairOfInts{1, 2};
-
-    mc::pair pairOfStrings{std::string("Hello"), std::string("World!")};
-
-    std::vector<mc::pair<std::string, int>> people{
-        mc::pair<std::string, int>{"Marnix", 20},
-        mc::pair<std::string, int>{"Camiel", 20}
-    };
-
-    std::cout << "People: " << std::endl;
-
-    for (auto& person : people) {
-        std::cout << person << std::endl;
-    }
-
-    // This won't work! The types are different for both first and second
-    // pairOfInts.swap();
-
-    // This will work fine on the other hand
-    std::cout << "Swap print: " << mc::pair<int, int>{1, 20}.swap() << std::endl;
-
-    // Test our vector!
-    mc::vector<std::string> vec{};
-    vec.push_back("Hello");
-    vec.push_back("Alice.");
-    vec.push_back("How");
-    vec.push_back("are");
-    vec.push_back("you");
-    vec.push_back("today?");
-
-    std::cout << "Vector print: " << vec << std::endl;
-
-    mc::pair<std::string, int> marnix{"Marnix", 20};
-    mc::pair<std::string, int> camiel{"Camiel", 20};
-
-    mc::pair<int, std::string> setie;
-
-    mc::pair<std::string, int> pairArray[10];
-
-    pairArray[1] = camiel;
-
-    std::cout << pairArray[1] << std::endl;
-
-    mc::vector<mc::pair<std::string, int>> set;
-
-    set.push_back(mc::pair<std::string, int>{"One", 1});
-    set.push_back(mc::pair<std::string, int>{"Two", 2});
-
-    // Delete the second pair out of our vector
-    set.erase(1);
-    std::cout << set << std::endl;
-
-    // Fill an array with random numbers and sort them
-    mc::vector<uint32_t> unsorted;
-
-    // Use mersenne twister to generate random numbers, see our previous topic
-    // For more information about the working of the mersenne twister
-    std::mt19937 mtw;
-    mtw.seed((int)time(nullptr));
- 
-    int iterations = 100000;
-
-    std::cout << "Iterations: " << iterations << std::endl;
-
-    for (int i = 0; i < iterations; i++) {
-        unsorted.push_back(mtw());
-    }
-
-    auto start = std::chrono::high_resolution_clock::now();
-
-    unsorted.sort();
-
-    auto finish = std::chrono::high_resolution_clock::now();
-    std::cout << "Sorted " << unsorted.size() << " elements in " << std::chrono::duration_cast<std::chrono::milliseconds>(finish-start).count() << " ms\n";
-
-    return 0;
-
-    std::pair<int,int> a{1,2};
-
-    auto s = a.first;
 }
